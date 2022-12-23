1d25205eca2f44b9506c83cf1e6e48ceee6be05e
andrupax94 <52213160+andrupax94@users.noreply.github.com>
2022-12-23 14:16:48 +0000

    dsfsdf
    sdfsd

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 cp /home/site/ext/composer.phar /usr/local/bin/composer
 cp /home/site/wwwroot/default /etc/nginx/sites-available/default
 service nginx restart
-cd /home/site/wwwroot && cp -n .env.example .env 
+cd /home/site/wwwroot/laravel && cp -n .env.example .env 
 php artisan key:generate
